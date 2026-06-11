d8dc81e31d9fe055a217434e7aa1f29c849f21e6
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-06-11 05:00:24 -0700

    admin second v

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -8,74 +8,70 @@ const Admin = () =>{
            <Profile/>
            <div className="grid grid-cols-1 md:grid-cols-5 gap-4 h-[150vh] px-5 md:px-10">
               {/**  admin profile*/}
-             <aside className="w-full border border-black/60">
+             <aside className="w-full py-20">
+                
 
              </aside>
               {/* main */}
            <div className="col-start-2 row-start-1 col-span-3 pt-20 px-2">
-              <article className="border p-2 space-y-4 ">
+              <article className="p-2 space-y-4 shadow-md rounded-md border py-5">
                   <div className="flex flex-col md:flex-row gap-4 justify-between w-full">
                   {/*product details */}
-                  <div className="w-full md:w-fit"> 
-                    <h1 className="mx-auto font-semibold my-2 text-center">Product Details</h1>
-                    <div className="w-full md:w-fi inline-flex gap-4 border p-2">
+                  <div className="w-full md:w-fit "> 
+                    <h1 className="mx-auto text-[20px] font-bold my-2 text-center my-5">Product Details</h1>
+                    <div className="w-full md:w-fi inline-flex gap-4 p-2  border-t">
                         <img src="https://images.unsplash.com/photo-1584917865442-de89df76afd3?q=80&w=1200&auto=format&fit=crop" 
                              alt="" 
-                             className="w-15 h-20 rounded-md" 
+                             className="h-20 rounded-md" 
                         /> 
-                        <div className="w-full text-[14px] text-gray-900">
-                          <p className="">Name  : <strong className="">Luxury Leather Tote</strong> </p>
-                          <p className="">ID  : <strong className="">TX0RT4-0095R</strong> </p>
-                          <p className="">Size  : <strong className="">M</strong> </p>
-                          <p className="">Color : <strong className="">Black</strong> </p>
-                          <p className="">Quantity : <strong className="">4</strong></p>
+                        <div className="w-full text-[15px] text-gray-900 space-y-2">
+                          <p className="">Name  : <b className="">Luxury Leather Tote</b> </p>
+                          <p className="">ID  : <b className="">TX0RT4-0095R</b> </p>
+                          <p className="">Size  : <b className="">M</b> </p>
+                          <p className="">Color : <b className="">Black</b> </p>
+                          <p className="">Quantity : <b className="">4</b></p>
                         </div>
  
                         </div>
                     </div>
                   {/**user details */}
                   <div className="w-full md:w-fit"> 
-                    <h1 className="mx-auto font-semibold my-2 text-center ">Customer Details</h1>
-                    <div className="w-full inline-flex gap-4 border p-2">
-                        <div className="text-[14px] text-gray-900">
-                          <p className="">Name  : <strong className="">Chisome Simkonda</strong> </p>
-                          <p className="">Phone  : <strong className="">0997914433</strong> </p>
-                          <p className="">Email : <strong className="">Simkonda@gmail.com</strong> </p>
-                          <p className="">Location : <strong className="">Mzuzu</strong></p>
-                          <p className="">Courier : <strong className="">CTS</strong></p>
-                        </div>
- 
+                    <h1 className="mx-auto text-[20px] font-bold  my-2 text-center my-5">Customer Details</h1>
+                        <div className="w-full text-[15px] text-gray-900 space-y-2 border-t p-2">
+                          <p className="">Name  : <b className="">Chisome Simkonda</b> </p>
+                          <p className="">Phone  : <b className="">0997914433</b> </p>
+                          <p className="">Email : <b className="">Simkonda@gmail.com</b> </p>
+                          <p className="">Location : <b className="">Mzuzu</b></p>
+                          <p className="">Courier : <b className="">CTS</b></p>
                         </div>
                     </div>
 
                   {/**time stump */}
                   <div className="w-full md:w-fit"> 
-                    <h1 className="mx-auto font-semibold my-2 text-center">Time Stump</h1>
-                    <div className="w-full inline-flex gap-4 border p-2">
-                        <div className="text-[14px] text-gray-900">
-                          <p className="">Date  : <strong className="">15/06/2026</strong> </p>
-                          <p className="">Time  : <strong className="">02:12:45:03</strong> </p>
-                        </div>
-                        </div>
+                    <h1 className="mx-auto text-[20px] font-bold  my-2 text-center my-5">Time Stump</h1>
+                     <div className="w-full gap-4 p-2 space-y-2 border-t">
+                          <p className="">Date  : <b className="">15/06/2026</b> </p>
+                          <p className="">Time  : <b className="">02:12:45:03</b> </p>
+                      </div>
                     </div>
                 </div>
                 {/*Receipts/payment/delivery status */}
-                <div className="w-full inline-flex justify-between">
-                    <span className="inline-flex flex-wrap gap-3"> 
+                <div className="w-full inline-flex justify-between border-t py-5">
+                    <span className="inline-flex flex-wrap gap-3 text-[1.1rem]"> 
                         Amount paid : <strong className="">MWK 450,000</strong>
-                        <button className="border inline-flex items-center gap-2">
+                        <button className="border inline-flex items-center gap-2 px-2 bg-black text-white font-semibold">
                             Receipt
                             <FiChevronDown className="size-5"/>
                         </button>
                     </span>
                     <span className="">
-                        Delivery : <strong className="text-red-400">Not delivered</strong>
+                        Delivery : <strong className="text-red-400">Pending...</strong>
                     </span>
                 </div>
               </article>
            </div>
            
-           <aside className="w-full border border-black/60">
+           <aside className="w-full">
 
              </aside>
            </div>
